2239b6b0b4eb18cdaf5f7f4d783bbbca50923854
chinmayjatana <chinmayjatana@gmail.com>
2017-08-09 21:19:26 -0700

    Update SmsAutoReader.java

--- SupportLibraries/SmsAutoReader/SmsAutoReader.java
+++ SupportLibraries/SmsAutoReader/SmsAutoReader.java
@@ -1,48 +1,44 @@
-import android.app.Dialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
-import android.view.View;
-import android.view.ViewGroup;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ *  This is SmsAutoReader class which is used to auto read the sms and load the url with in it .
+ */
 
 public class SmsAutoReader {
     private static Context con;
-    
-    
-    private static final String key1 ="2291";
-    private static final String key2 ="AH-MCONNT";
-    private static final String key3 ="ADMCONNT";
-    private static final String key4 ="";
-    private static final String key5 ="";
-    private static final String key6 ="";
     private static IntentFilter filter;
+    private static final String[] keys ={"2291","AH-MCONNT","ADMCONNT"};
     
-    
-    private static boolean a=false;
+    //status will be true if the broadcast receiver is on
+    //status will be false if the broadcast receiver is off
+    private static boolean status=false;
 
-    // Constructor of SmsAutoReader Class 
-    // Pass Activity Instance to the SmsAutoReader in order to use its methods 
+    // Constructor of SmsAutoReader Class
+    // Pass Activity Instance to the SmsAutoReader in order to use its methods
+
     public SmsAutoReader(Context con) {
         this.con = con;
         this.filter = new IntentFilter("android.provider.Telephony.SMS_RECEIVED");
     }
 
-    
-    // BroadcastReceiver to receive the messages 
+    // BroadcastReceiver to receive the messages
     private final BroadcastReceiver myReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            a=true;
+
+
             Bundle bundle = intent.getExtras();
             SmsMessage[] msgs = null;
             String str = "";
@@ -53,12 +49,13 @@ public class SmsAutoReader {
                 for (int i = 0; i < msgs.length; i++) {
                     msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
                     str += "SMS from " + msgs[i].getOriginatingAddress();
-                    str += " :";
+                    str += "-->";
                     str += msgs[i].getMessageBody().toString();
                     str += "\n";
                 }
-                if(str.contains(key1) || str.contains(key2) || str.contains(key3)) {
-                   // The link is passing on to the Web method  
+//---display the new SMS message---
+
+                if(Arrays.asList(keys).contains(str.substring(str.indexOf("SMS from ") + 9, str.indexOf("-->")))) {
                    Web(pullLinks(str));
                 }
             }
@@ -66,31 +63,29 @@ public class SmsAutoReader {
     };
 
     // After receiving the code invoke this method to stop the BroadcastReceiver
-    // Also invoke this method in the onDestroy() method of the Activity
+    // Do'nt try to invoke this method dynamically that means by the action of any button
+    
+
     public void StopSmsAutoReader()
     {
-        if(a){
+        if(status){
             con.unregisterReceiver(myReceiver);
-            a=false;
+            status=false;
         }
 
     }
-
     // Invoke this method in order to start the SmsAutoReader
-    
     public void StartSmsAutoReader()
     {
+        status=true;
         con.registerReceiver(myReceiver,filter);
     }
 
     // This method is used to load the url
-    
+
     private void Web(String url)
     {
-        final Dialog dialog = new Dialog(con,android.R.style.Theme_DeviceDefault_Light_NoActionBar_Fullscreen);
         WebView wv = new WebView(con);
-        wv.setVisibility(View.INVISIBLE);
-        wv.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT));
         wv.loadUrl(url);
         WebSettings webSettings = wv.getSettings();
         webSettings.setJavaScriptEnabled(true);
@@ -101,18 +96,11 @@ public class SmsAutoReader {
 
                 return false;
             }
-
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                dialog.dismiss();
-            }
         });
-        dialog.setContentView(wv);
-        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
-        dialog.show();
     }
 
-//  This method is used to pull link from Sms String  
+
+    //  This method is used to pull link from Sms String
     
     private String pullLinks(String text) {
         String link = "";
